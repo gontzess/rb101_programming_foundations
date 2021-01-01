fb261c6307fad5cbfa59f1121d857f0d0782ace2
Stephen Gontzes <gontzess@gmail.com>
2021-01-01 17:13:46 -0500

    lesson progress jan 01

---
+++ lesson_02/psuedo_code.rb
@@ -0,0 +1,55 @@
+# Writing Psuedo Code Practice
+
+# 1) a method that returns the sum of two integers
+
+=begin
+Given two integers.
+
+Add the integers together.
+
+Return the sum.
+=end
+
+=begin
+START
+# Given two integers.
+
+SET integer1
+SET integer2
+SET sum equal to integer1 + integer2
+RETURN sum
+END
+=end
+
+# 2) a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+=begin
+Given an array of strings.
+
+Take each element and concatenate into a new string
+
+Return new string
+=end
+
+=begin
+START
+Given Array of strings.
+
+SET new_string
+
+For each element in Array:
+  concatenate element onto new_string
+
+RETURN new_string
+=end
+
+# 3) a method that takes an array of integers, and returns a new array with every other element
+
+=begin
+Given an array of integers.
+
+
+=end
+
+=begin
+=end
