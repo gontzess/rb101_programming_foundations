df68f9e732c8366069828fe43b800d4b1731b721
Stephen Gontzes <gontzess@gmail.com>
2021-01-24 16:42:17 -0500

    exercises progress jan 24

--- exercises/easy_5/daily_double.rb
+++ exercises/easy_5/daily_double.rb
@@ -1,8 +1,15 @@
 # Write a method that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character. You may not use String#squeeze or String#squeeze!.
 
 def crunch(phrase)
-  #phrase.gsub(/.{2,}?/) { |dupes| dupes[0] }
-  phrase.gsub(/..+?/) { |dupes| dupes[0] }
+  cleaned_chars = []
+  phrase.each_char do |char|
+    cleaned_chars.push(char) unless cleaned_chars.last == char
+  end
+  cleaned_chars.join
+end
+
+def crunch_regexp(phrase)
+  phrase.gsub(/(.)\1+/, '\1')
 end
 
 puts crunch('ddaaiillyy ddoouubbllee') == 'daily double'
@@ -16,3 +23,16 @@ puts crunch('4444abcabccba')
 puts crunch('ggggggggggggggg')
 puts crunch('a')
 puts crunch('')
+
+
+puts crunch_regexp('ddaaiillyy ddoouubbllee') == 'daily double'
+puts crunch_regexp('4444abcabccba') == '4abcabcba'
+puts crunch_regexp('ggggggggggggggg') == 'g'
+puts crunch_regexp('a') == 'a'
+puts crunch_regexp('') == ''
+
+puts crunch_regexp('ddaaiillyy ddoouubbllee')
+puts crunch_regexp('4444abcabccba')
+puts crunch_regexp('ggggggggggggggg')
+puts crunch_regexp('a')
+puts crunch_regexp('')
