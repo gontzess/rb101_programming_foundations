17736b4785e790210d3262eedc9024e8baa5807f
Stephen Gontzes <gontzess@gmail.com>
2021-02-13 13:25:20 -0500

    twenty one game updates

--- lesson_06/twenty_one/twenty_one.rb
+++ lesson_06/twenty_one/twenty_one.rb
@@ -1,25 +1,15 @@
 # twenty-one game
 
-# 1. Initialize deck
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
-
 CARD_SUITS = %w(Hearts Diamonds Clubs Spades)
 CARD_NAMES = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
 CARD_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [11, 1]]
 CARDS = (1..52).each_with_object({}) do |card_id, hash|
-          hash[card_id] = { name: CARD_NAMES[(card_id % 13) - 1],
-                             value: CARD_VALUES[(card_id % 13) - 1],
-                             suit: CARD_SUITS[(card_id - 1) / 13] }
-        end
+  hash[card_id] = { name: CARD_NAMES[(card_id % 13) - 1],
+                    value: CARD_VALUES[(card_id % 13) - 1],
+                    suit: CARD_SUITS[(card_id - 1) / 13] }
+end
+CARD_LIMIT = 21
 ROUNDS_TO_WIN = 3
-# NUMBER_OF_PLAYERS = 2
 
 ## METHOD DEFINITIONS
 def clear_screen
@@ -54,33 +44,29 @@ def initialize_deck
 end
 
 def initialize_table
-  { Player: [], Dealer:[], deck: initialize_deck }
+  { Player: [], Dealer: [], deck: initialize_deck,
+    total: { Player: 0, Dealer: 0 } }
 end
 
 def initialize_scoreboard
-  { round_score: { Player: 0, Dealer: 0 },
-    round: 1,
+  { round: 1,
     game_score: { Player: 0, Dealer: 0 },
     game_complete: false,
-    game_winner: '' }
+    game_winner: nil }
 end
 
-def reset_round_score!(scoreboard)
-  scoreboard[:round_score] = { Player: 0, Dealer: 0 }
-end
-
-def update_round_score!(table, scoreboard)
-  scoreboard[:round_score] = { Player: smart_sum_of_hand(table, 'Player'),
-                               Dealer: smart_sum_of_hand(table, 'Dealer') }
+def update_totals!(table)
+  table[:total] = { Player: smart_sum_of_hand(table, 'Player'),
+                    Dealer: smart_sum_of_hand(table, 'Dealer') }
+  nil
 end
 
 def update_round_results!(scoreboard, round_winner)
   return nil unless %w(Player Dealer tie).include?(round_winner)
 
-  if round_winner == 'tie'
-    scoreboard[:round] += 1
-  else  # i.e. Player or Dealer won
-    scoreboard[:round] += 1
+  scoreboard[:round] += 1
+
+  if round_winner != 'tie' # i.e. Player or Dealer won
     scoreboard[:game_score][round_winner.to_sym] += 1
 
     if scoreboard[:game_score][round_winner.to_sym] >= ROUNDS_TO_WIN
@@ -88,16 +74,14 @@ def update_round_results!(scoreboard, round_winner)
       scoreboard[:game_winner] = round_winner
     end
   end
-
-  reset_round_score!(scoreboard)
 end
 
-def bust?(sum)
-  sum > 21
+def busted?(sum)
+  sum > CARD_LIMIT
 end
 
-def player_bust?(scoreboard, player)  ## CONSOLIDATE
-  scoreboard[:round_score][player.to_sym] > 21
+def player_busted?(table, player)
+  busted?(table[:total][player.to_sym])
 end
 
 def raw_sum_of_hand(table, player)
@@ -107,7 +91,7 @@ def raw_sum_of_hand(table, player)
             else
               table[player.to_sym].inject(0) do |total, card_idx|
                 if CARDS[card_idx][:name] == 'Ace'
-                  aces +=1
+                  aces += 1
                   total + CARDS[card_idx][:value][0]
                 else
                   total + CARDS[card_idx][:value]
@@ -121,23 +105,108 @@ def smart_sum_of_hand(table, player)
   raw_sum, aces = raw_sum_of_hand(table, player)
 
   loop do
-    return raw_sum if aces == 0 || !bust?(raw_sum)
+    return raw_sum if aces == 0 || !busted?(raw_sum)
 
     raw_sum -= 10
     aces -= 1
   end
 end
 
-# def determine_round_winner_OLD(scoreboard)
-#   winner = scoreboard[:round_score].keys.max_by do |player|
-#              scoreboard[:round_score][player]
-#            end
-#   winner.to_s
-# end
+def deal_first_round!(table)
+  2.times do
+    table[:Player] << table[:deck].shift
+    table[:Dealer] << table[:deck].shift
+  end
+  update_totals!(table)
+end
 
-def determine_round_winner(scoreboard)
-  result = scoreboard[:round_score][:Player] <=>
-           scoreboard[:round_score][:Dealer]
+def convert_card_ids(ary_card_ids, output_key)
+  # accepts output_key = 'name', 'value', or 'suit'
+  return nil unless %w(name value suit).include?(output_key)
+
+  ary_card_ids.each_with_object([]) do |card_id, converted|
+    converted << CARDS[card_id][output_key.to_sym]
+  end
+end
+
+def display_table(table, player='both')
+  # accepts player = 'both', 'Player', or 'Dealer'round_winner
+  dealer_cards = ['unknown card'] +
+                 convert_card_ids(table[:Dealer][1..-1], 'name')
+  player_cards = convert_card_ids(table[:Player], 'name')
+
+  case player
+  when 'both'
+    prompt('Dealer has:', joinor(dealer_cards))
+    prompt('You have:', joinor(player_cards))
+  when 'Player'
+    prompt('You have:', joinor(player_cards))
+  when 'Dealer'
+    prompt('Dealer has:', joinor(dealer_cards))
+  else
+    prompt('ERROR')
+  end
+end
+
+def hit!(table, player)
+  table[player.to_sym] << table[:deck].shift
+  nil
+end
+
+def dealer_hit?(table)
+  return false if player_busted?(table, 'Player')
+
+  table[:total][:Dealer] < table[:total][:Player]
+end
+
+def player_turn!(table)
+  puts ''
+  prompt("Your turn...")
+  loop do
+    answer = ''
+    prompt("Hit or stay? (h/s)")
+
+    loop do
+      answer = gets.chomp.downcase
+      break if %w(h s).include?(answer)
+      prompt("That's not a valid choice.")
+    end
+
+    if answer == 's'
+      prompt("You stayed at #{table[:total][:Player]}.")
+      break
+    end
+
+    hit!(table, 'Player')
+    update_totals!(table)
+    display_table(table, 'Player')
+
+    break if player_busted?(table, 'Player')
+  end
+end
+
+def dealer_turn!(table)
+  puts ''
+  prompt("Dealer turn...")
+
+  loop do
+    if dealer_hit?(table)
+      hit!(table, 'Dealer')
+      update_totals!(table)
+      prompt("Dealer hits!")
+      display_table(table, 'Dealer')
+    else
+      prompt("Dealer stays at #{table[:total][:Dealer]}.")
+      break
+    end
+
+    break if player_busted?(table, 'Dealer')
+  end
+end
+
+def determine_winning_hand(table)
+  result = table[:total][:Player] <=> table[:total][:Dealer]
+
   case result
   when -1 then 'Dealer'
   when 1  then 'Player'
@@ -146,93 +215,83 @@ def determine_round_winner(scoreboard)
   end
 end
 
-def deal_first_round!(table, scoreboard)
-  2.times do
-    table[:Player] << table[:deck].shift
-    table[:Dealer] << table[:deck].shift
-  end
-  update_round_score!(table, scoreboard)
+def display_game_start
+  clear_screen
+  prompt("Welcome to the game!")
+  prompt("You will be playing against the Dealer.")
+  prompt("Get as close to #{CARD_LIMIT} as possible, without going over.")
+  prompt("First to #{ROUNDS_TO_WIN} wins the game.")
+  puts ''
+  prompt("Ready? Press enter to begin.")
+  gets.chomp
+  clear_screen
 end
 
-def convert_card_ids(ary_card_ids, output_key)  # accepts output_key = 'name', 'value', or 'suit'
-  return nil unless %w(name value suit).include?(output_key)
-
-  ary_card_ids.each_with_object([]) do |card_id, converted|
-    converted << CARDS[card_id][output_key.to_sym]
-  end
+def display_game_end
+  prompt("Press enter to exit.")
+  gets.chomp
+  prompt("Goodbye!")
+  sleep 3
+  clear_screen
 end
 
-def display_table(table, player='both')  # accepts player = 'both', 'Player', or 'Dealer'
-  dealer_cards = ['unknown card'] + convert_card_ids(table[:Dealer][1..-1], 'name')
-  player_cards = convert_card_ids(table[:Player], 'name')
-
-  case player
-  when 'both'
-    prompt('Dealer has:', joinor(dealer_cards))
-    # prompt('pssstt.. secret card is:', convert_card_ids(table[:Dealer], 'name').first)  # remove later
-    p smart_sum_of_hand(table, 'Dealer')  # remove later
-    prompt('You have:', joinor(player_cards))
-    p smart_sum_of_hand(table, 'Player')  # remove later
-  when 'Player'
-    prompt('You have:', joinor(player_cards))
-    p smart_sum_of_hand(table, 'Player')  # remove later
-  when 'Dealer'
-    prompt('Dealer has:', joinor(dealer_cards))
-    p smart_sum_of_hand(table, 'Dealer')  # remove later
-  else
-    prompt('ERROR')
-  end
+def display_round_num(scoreboard)
+  scoreboard[:round]
 end
 
-def hit!(table, player)
-  table[player.to_sym] << table[:deck].shift
-  return nil
-end
-
-def computer_hit?(scoreboard)
-  scoreboard[:round_score][:Dealer] < 17
-end
-
-def player_turn!(table, scoreboard)
-  loop do
-    answer = ''
-    prompt('Your turn: hit or stay? (h/s)')
-
-    loop do
-      answer = gets.chomp.downcase
-      break if %w(h s).include?(answer)
-      prompt("That's not a valid choice.")
-    end
-
-    break if answer == 's'
-
-    hit!(table, 'Player')
-    update_round_score!(table, scoreboard)
-    display_table(table, 'Player')
-
-    break if player_bust?(scoreboard, 'Player')
-  end
-end
-
-def dealer_turn!(table, scoreboard)
-  loop do
-    if computer_hit?(scoreboard)
-      hit!(table, 'Dealer')
-      update_round_score!(table, scoreboard)
-      prompt('Dealer hits!')
-      display_table(table, 'Dealer')
+def display_round_results(table, round_winner, busted)
+  puts ''
+  if round_winner == 'Player'
+    if busted
+      prompt("The dealer busts at #{table[:total][:Dealer]}.. you win.")
     else
-      prompt('Dealer stays!')
-      break
+      prompt("You win.. this time..")
     end
-    break if player_bust?(scoreboard, 'Dealer')
+  elsif round_winner == 'Dealer'
+    if busted
+      prompt("YA BUSTED at #{table[:total][:Player]}. Dealer wins!")
+    else
+      prompt("YOU LOSE. Dealer wins!")
+    end
+  else
+    prompt("It's a tie.")
+  end
+end
+
+def game_score(scoreboard)
+  score = []
+  scoreboard[:game_score].each do |player, wins|
+    name = player == :Player ? 'You' : player
+    score << "#{name}: #{wins}"
+  end
+  score.join(', ')
+end
+
+def display_game_score(scoreboard)
+  puts ''
+  prompt("The score is #{game_score(scoreboard)}")
+end
+
+def determine_game_winner(scoreboard)
+  scoreboard[:game_winner]
+end
+
+def game_won?(scoreboard)
+  !!determine_game_winner(scoreboard)
+end
+
+def display_game_results(game_winner)
+  if game_winner == 'Player'
+    prompt("Congratulations, you won the game.. beginner's luck..")
+  else
+    prompt("Remember, Dealer always wins!")
   end
 end
 
 def play_again?
   answer = ''
   loop do
-    prompt('Another round? (y/n)')
+    prompt("Another round? (y/n)")
     answer = gets.chomp.downcase
     break if %w(y n).include?(answer)
     prompt("That's not a valid choice.")
@@ -241,40 +300,43 @@ def play_again?
 end
 
 ## MAIN PROGRAM
-clear_screen
+display_game_start
 scoreboard = initialize_scoreboard
-prompt('Welcome!')
 
 loop do
+  display_round_num(scoreboard)
   table = initialize_table
-  deal_first_round!(table, scoreboard)
+  deal_first_round!(table)
   display_table(table)
 
-  player_turn!(table, scoreboard)
+  player_turn!(table)
 
-  if player_bust?(scoreboard, 'Player')
+  if player_busted?(table, 'Player')
+    busted = true
     round_winner = 'Dealer'
-    prompt('Ya BUST! Dealer wins!')
   else
-    dealer_turn!(table, scoreboard)
+    dealer_turn!(table)
 
-    if player_bust?(scoreboard, 'Dealer')
+    if player_busted?(table, 'Dealer')
+      busted = true
       round_winner = 'Player'
-      prompt('The dealer busts.. you win.')
     else
-      round_winner = determine_round_winner(scoreboard)
-      case round_winner
-      when 'Player' then prompt('You win.. this time..')
-      when 'Dealer' then prompt('You LOSE! Dealer wins!')
-      else               prompt("It's a tie.")
-      end
+      busted = false
+      round_winner = determine_winning_hand(table)
     end
   end
 
+  display_round_results(table, round_winner, busted)
   update_round_results!(scoreboard, round_winner)
+  display_game_score(scoreboard)
 
-  break if !play_again?
+  break if game_won?(scoreboard) || !play_again?
   clear_screen
 end
 
-prompt('Goodbye!')
+if game_won?(scoreboard)
+  game_winner = determine_game_winner(scoreboard)
+  display_game_results(game_winner)
+end
+
+display_game_end
